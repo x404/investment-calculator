dd2ed1b53bd40900a36dae29ef32719e53611d69
x404 <kronos2003@gmail.com>
2024-09-03 14:58:03 +0300

    feat: added components header, userinput. made calculation formula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
+import type { InvestmentInput } from "./investment-input.model";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    HeaderComponent,
+    UserInputComponent
+  ]
 })
 export class AppComponent {
-  title = 'investment-calculator';
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    console.log(annualData)
+
+    return annualData;
+  }
+
 }
---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,7 @@
+export interface InvestmentInput {
+  initialInvestment: number,
+  duration: number,
+  expectedReturn: number,
+  annualInvestment: number
+
+}
